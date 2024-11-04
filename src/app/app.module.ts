2922570c8646e6615162d2907f97ad4abe47237e
candi <alcantarilla.cacan24@gmail.com>
2024-11-04 11:51:08 +0100

    creación de componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListGasComponent } from './components/list-gas/list-gas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListGasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/list-gas/list-gas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-gas',
+  templateUrl: './list-gas.component.html',
+  styleUrl: './list-gas.component.css'
+})
+export class ListGasComponent {
+
+}
