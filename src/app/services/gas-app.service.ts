b6ed3769955ccf8d4454f8693677cf80e85d7ab5
Candi Alcantarilla <alcantarilla.cacan24@triana.salesianos.edu>
2024-11-05 21:20:51 +0100

    parseo completado, falta terminar de hacer los card

--- src/app/services/gas-app.service.ts
+++ src/app/services/gas-app.service.ts
@@ -1,16 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { GasListResponse } from '../models/gas-app.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GasAppService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getGasList(): Observable<GasListResponse> {
-    return this.http.get<GasListResponse>('https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/');
+  getGasList() {
+    return this.http.get('https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/');
   }
 }
